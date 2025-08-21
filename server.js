0c69b7b74dd8900fde617f410aa7bd22d7a16db6
Selvabala4639 <166098611+Selvabala4639@users.noreply.github.com>
2025-08-21 14:01:40 +0530

    Update server.js

--- server.js
+++ server.js
@@ -136,9 +136,10 @@ app.get('*', (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 initializeDatabase().then(() => {
-  app.listen(port, () => {
+  app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
   });
 });
+
 
 
